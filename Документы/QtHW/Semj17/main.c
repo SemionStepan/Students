ffb8e658624716692300afe908e4c1654768308f
Semyon Stepanchenko <genji2214@mail.ru>
2022-01-12 15:12:08 +0300

    Finished program for deleting spaces
    *added new variable for string in cicle

--- Документы/QtHW/Semj17/main.c
+++ Документы/QtHW/Semj17/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char str [] = {"О сколько нам открытий чудных Готовят просвещенья дух И опыт, сын ошибок трудных, И гений, парадоксов друг, И случай, бог изобретатель. А. С. Пушкин."};
+
+int DeleteSpace(char* str);
 
 int main()
 {
-    printf("Hello World!\n");
+    DeleteSpace(str);
     return 0;
+}
+int DeleteSpace(char* str)
+{
+    int strstrlen = strlen(str);
+
+    int j = 0;
+
+    char nospacesstr[strstrlen];
+
+    for(int count = 0; count < strstrlen ;count++)
+    {
+        if(str[count] !=' ')
+        {
+            nospacesstr[j] = str[count];
+            j++;
+        }
+    }
+  return printf("%s",nospacesstr);
 }
